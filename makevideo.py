dc6e8af9aa61abb1849664bf5f90e0bb0cf488d0
s4575321 <s4575321@moss.labs.eait.uq.edu.au>
2020-09-05 15:43:22 +1000

    fix bug

--- makevideo.py
+++ makevideo.py
@@ -29,15 +29,14 @@ wolf_im = preprocessing_img(wolf_path, step, 2)
 # save number of plot
 # print(T)
 # print("T, count ",T, len(count))
-# subsample = 1
-# cc = []
-# for t in range(0,T,subsample):
-#     cc.append(count[t*3])
-#     cc.append(count[1+t*3])
-#     cc.append(count[2+t*3])
-# T = len(cc)//3
-# count = cc
-save_number_trend_plot(count,N,T,fps, plot_path)
+subsample = T//100
+cc = []
+for t in range(0,T,subsample):
+    cc.append(count[t*3])
+    cc.append(count[1+t*3])
+    cc.append(count[2+t*3])
+count = cc
+save_number_trend_plot(count,N,len(cc)//3,fps, plot_path)
 
 # save visualization
 cnt = 0
@@ -49,16 +48,15 @@ while count > 0:
     else:
         tt = count
         count = 0
-    # print(count)
+    print(count)
     canvas_v = np.zeros((tt,outWidth,outWidth,3))
     for t in range(tt):
-        # pool.map(draw_one_canvas, zip(video_paths, means, stds, periods))
         canvas = np.zeros((outWidth,outWidth,3))
         draw_one_canvas(canvas, sheep[(t*N):((t+1)*N)], sheep_im,  wolve[(t*N):((t+1)*N)], wolf_im, grass[(t*N):((t+1)*N)], outWidth, step,realW)
         canvas = cv2.cvtColor(canvas.astype(np.uint8), cv2.COLOR_BGR2RGB)
         canvas_v[t,...] = canvas
     canvas_v = canvas_v.astype(np.uint8)
     write_video(canvas_v.transpose((3,0,1,2)),visual_path+"_"+str(cnt)+".avi",outWidth, fps)
-    print("write out {}", visual_path+"_"+str(cnt)+".avi")
+    print("write out", visual_path+"_"+str(cnt)+".avi")
     cnt+= 1
 
